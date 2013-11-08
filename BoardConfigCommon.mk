a37a29885f148c6c1ab4d8aebe545647b6fccd17
kejar31 <justin.a.rogers@gmail.com>
2013-11-08 01:23:26 -0600

    jf: don't build qcom camera HAL

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,4 +82,6 @@ BOARD_USES_SEPERATED_VOIP := true
 # Use seperate devices for 3-pole headset
 BOARD_USES_SEPERATED_HEADSET_MIC := true
 
+# Don't build qcom camera HAL 	72
+USE_DEVICE_SPECIFIC_CAMERA := true
 
